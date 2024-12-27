4e07a3cf42684b09ed848d1e832c7bc38a393f0d
HyeongAn <guddks84@gmail.com>
2024-12-27 23:29:34 +0900

    fix merge conflict

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,14 +1,21 @@
 import { useRecoilValue } from "recoil"
-import { elementsState } from "@/features/elements"
 import { Layout } from "@/widgets/layouts"
-import { Elements } from "@/features/elements"
+import { Header } from "@/widgets"
+import { Elements, elementsState } from "@/features/elements"
+import { cn } from "@/shared/lib"
+import { useDownloadAsSvg } from "@/shared/model"
 
 export const MainPage = () => {
   const elements = useRecoilValue(elementsState)
 
+  const { ref, downloadAsPng } = useDownloadAsSvg()
+
   return (
     <Layout>
-      <Elements elements={elements} />
+      <Header downloadAsSvg={downloadAsPng} />
+      <div ref={ref} className={cn("flex flex-nowrap gap-2 bg-slate-100 p-2")}>
+        <Elements elements={elements} />
+      </div>
     </Layout>
   )
 }
